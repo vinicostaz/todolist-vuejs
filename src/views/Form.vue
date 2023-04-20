17dead05548c11577f128c9b98db14f7139f7925
vinicostaz <vinicius.fonseca132@gmail.com>
2023-04-20 11:00:59 -0300

    forms components created

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,5 +1,50 @@
 <template>
-  <div>
-    <h1>Form</h1>
-  </div>
+  <div class="container mt-2">
+    <b-form>
+      <b-form-group
+        label="Titulo"
+        label-for="subject"
+      >
+      <b-form-input
+        id="subject"
+        v-model="form.subject"
+        type="text"
+        placeholder="Ex: Lavar carro"
+        required
+        autocomplete="off"
+      ></b-form-input>
+    </b-form-group>
+
+    <b-form-group
+        label="Descrição"
+        label-for="description"
+      >
+      <b-form-textarea
+        id="description"
+        v-model="form.description"
+        type="text"
+        placeholder="Ex: Preciso levar o carro para lavar"
+        required
+        autocomplete="off"
+      ></b-form-textarea>
+    </b-form-group>
+    <b-button
+    type="submit" variant="outline-primary"> Salvar </b-button>
+  </b-form>
+</div>
 </template>
+
+<script>
+export default {
+  name: "Form",
+
+  data() {
+    return {
+      form: {
+        subject: "",
+        description: ""
+      }
+    }
+  }
+}
+</script>
