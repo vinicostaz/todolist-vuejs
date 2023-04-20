3f2480fc69a752eb6238ff9033c39449a8500e46
vinicostaz <vinicius.fonseca132@gmail.com>
2023-04-20 11:23:40 -0300

    list tasks created

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="container mt-2">
-    <b-card title="Título">
-      <b-card-text>descrição</b-card-text>
+    <div v-for="(task, index) in tasks" :key="index">
+    <b-card :title="task.subject" class="mb-2">
+      <b-card-text>{{task.description }}</b-card-text>
 
       <b-button variant="outline-secondary" class="mr-2"> Editar </b-button>
       <b-button variant="outline-danger" class="mr-2"> Excluir </b-button>
       </b-card>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "List",
+
+  data() {
+    return {
+      tasks: []
+    }
+  },
+
+  created() {
+    this.tasks = (localStorage.getItem("tasks")) ? JSON.parse(localStorage.getItem("tasks")) : [];
+
+
+  }
+}
+</script>
