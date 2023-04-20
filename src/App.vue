05f2b2de01d5c7051b44371ea3cfddde38149779
vinicostaz <vinicius.fonseca132@gmail.com>
2023-04-20 10:26:43 -0300

    navbar created

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app">
+     <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">ToDoList</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Tarefas</b-nav-item>
+        <b-nav-item href="#">Formulário</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
